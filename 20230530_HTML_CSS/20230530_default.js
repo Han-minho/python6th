53152ee47626d20a1a8c22eca2d21abfd0364c71
Hanminho <hihi6024@gmail.com>
2023-05-30 13:11:28 +0900

    20230530 javascript 실습용 파일 내용 정리

--- 20230530_HTML_CSS/20230530_default.js
+++ 20230530_HTML_CSS/20230530_default.js
@@ -1,12 +1,14 @@
-document.write('Hello World!');
+/*document.write('Hello World!');
 document.write("<h1>Welcome to JS Program</h1>");
-document.write("<h2>Welcome to JS Program</h2>");
+document.write("<h2>Welcome to JS Program</h2>");*/
+/*
 
 console.log("Welcome JS Program log!");
 console.info("Welcome JS Program info!");
 console.warn("Welcome JS Program warn!");
 console.error("Welcome JS Program error!");
 console.log('hi');
+*/
 
 //alert("Welcome JS Program");
 //var a = prompt("Welcome JS Program");
@@ -22,7 +24,7 @@ console.log("123", typeof "123");
 console.log(true,typeof true);
 console.log(false,typeof false);*/
 
-var car;
+/*var car;
 console.log(car);
 var car="";
 console.log(car);
@@ -30,5 +32,14 @@ var person = {firstName:"John",lastName:"Doe",age:50,eyeColor:"blue"};
 
 console.log(typeof person,person);
 person = null;
-console.log(typeof person,person);
+console.log(typeof person,person);*/
+
+var name = "이승훈";
+var age = 29;
+var cgpa = 3.92;
+var lineBreak = "<br/>"
+
+document.write("이름 : " + name + lineBreak);
+document.write("나이 : " + age + lineBreak);
+document.write("학점 : " + cgpa + lineBreak);
 
