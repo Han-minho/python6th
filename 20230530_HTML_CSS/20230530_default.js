b48eec32447ee569638e347b3283fbfdd9f438c2
Hanminho <hihi6024@gmail.com>
2023-05-30 11:48:49 +0900

    20230530 javascript 실습용 파일 내용 추가

--- 20230530_HTML_CSS/20230530_default.js
+++ 20230530_HTML_CSS/20230530_default.js
@@ -1,1 +1,33 @@
-document.write('Hello World!');
+/*document.write('Hello World!');
+document.write("<h1>Welcome to JS Program</h1>");
+document.write("<h2>Welcome to JS Program</h2>");
+
+console.log("Welcome JS Program log!");
+console.info("Welcome JS Program info!");
+console.warn("Welcome JS Program warn!");
+console.error("Welcome JS Program error!");
+console.log('hi');*/
+
+//alert("Welcome JS Program");
+//var a = prompt("Welcome JS Program");
+/*console.log(a);
+
+var i = 0;
+console.log(i++);
+console.log(++i);*/
+
+/*console.log(123,typeof 123);
+console.log(123.5, typeof 123.5);
+console.log("123", typeof "123");
+console.log(true,typeof true);
+console.log(false,typeof false);*/
+
+var car;
+console.log(car);
+var car="";
+console.log(car);
+var person = {firstName:"John",lastName:"Doe",age:50,eyeColor:"blue"};
+
+console.log(typeof person,person);
+person = null;
+console.log(typeof person,person);
