6cab68a4bd10c2998138858c73e8f7ea2dc651a4
Hanminho <hihi6024@gmail.com>
2023-05-31 22:01:05 +0900

    20230531 제어구조 정리

---
+++ 20230531_HTML_CSS/age.py
@@ -0,0 +1,8 @@
+age = 25
+
+if age <13:
+    print("You are a child.")
+elif age < 18:
+    print("You are a teenager.")
+else:
+    print("You are ana adult.")
---
+++ 20230531_HTML_CSS/loop.py
@@ -0,0 +1,11 @@
+# for 문 예시
+print("For loop : ")
+for i in range(5):
+    print(f"Loop iteration: {i}")
+
+# while문 예시
+print("\nWhlie loop : ")
+counter = 0
+while counter < 5:
+    print(f"Loop iteration : {counter}")
+    counter+=1
