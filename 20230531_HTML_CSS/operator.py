1be301444d6047621ab5d9aa1020ab4025972df2
Hanminho <hihi6024@gmail.com>
2023-05-31 22:00:29 +0900

    20230531 연산자와 표현식 정리

---
+++ 20230531_HTML_CSS/operator.py
@@ -0,0 +1,30 @@
+# 변수 선연
+a = 10
+b = 3
+
+# 산술 연산자 예시
+addition = a+b
+subtraction = a-b
+multiplication = a*b
+division = a/b
+remainder = a%b
+
+# 비교 연산자 예시
+is_equal = a==b
+is_not_equal = a!=b
+is_greater = a>b
+is_less = a<b
+
+# 논리 연산자 예시
+and_result = is_equal and is_greater
+or_result = is_equal or is_greater
+not_result = not is_equal
+
+# 출력
+print("Arithmetic operators : ")
+print(f"Addition : {addition}, Subtraction : {subtraction},")
+print(f"Multiplication : {multiplication}, Division : {division}, Remainder : {remainder}\n")
+print("Comparison Operators")
+print(f"is_equal : {is_equal},is_not_equal : {is_not_equal},is_greater : {is_greater}, is_less : {is_less}\n")
+print("Logical Operators : ")
+print(f"and_result : {and_result}, or_result : {or_result}, not_result : {not_result}")
