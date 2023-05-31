4036bad55fac4ad50f14fc162af2cb8664ddc444
Hanminho <hihi6024@gmail.com>
2023-05-31 09:31:22 +0900

    20230531 비교연산자

--- 20230531_HTML_CSS/20230531_default.js
+++ 20230531_HTML_CSS/20230531_default.js
@@ -1,5 +1,26 @@
-var cels = parseFloat(prompt("섭씨 입력 : "));
+/*var cels = parseFloat(prompt("섭씨 입력 : "));
 var forn = cels * (9/5)+32;
 
-document.write("화씨 : "+forn);
+document.write("화씨 : "+forn);*/
+
+var num1 = 10;
+var num2 = 20;
+var num3 = "10";
+var num4 = 20;
+var num5 = 15;
+
+console.log("일반 크기비교");
+console.log(num1 > num2,num1, ">" ,num2);
+console.log(num1 >= num2,num1, ">=" ,num2);
+console.log(num1 < num2,num1, "<" ,num2);
+console.log(num1 <= num2,num1, "<=" ,num2);
+
+console.log("같은지 여부 확인")
+console.log(num1 == num4,num1, "==" ,num4);
+console.log(num1 != num4,num1, "!=" ,num4);
+
+console.log("===")
+console.log(num1 === num3,num1," === ",num3);
+console.log(num2 === num3,num2," === ",num3);
+console.log(num1 == num3,num1," == ",num3);
 
