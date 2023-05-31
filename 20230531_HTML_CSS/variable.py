43e5e177e7bac86d2cc64d5bae14a95b60bc4324
Hanminho <hihi6024@gmail.com>
2023-05-31 22:00:00 +0900

    20230531 변수와 자료형 정리

---
+++ 20230531_HTML_CSS/variable.py
@@ -0,0 +1,12 @@
+# 변수와 자료형 예시
+integer_var = 5
+float_var = 3.14
+string_var = "Hello"
+boolean_var = True
+
+# 변수 값 출력
+print(f"Integer : {integer_var}")
+print(f"Float : {float_var}")
+print(f"String : {string_var}")
+print(f"Boolean : {boolean_var}")
+
