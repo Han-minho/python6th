daff04842d66a8c2b8ba21673379b7c177060b45
Hanminho <hihi6024@gmail.com>
2023-05-31 09:37:37 +0900

    20230531 논리연산자

--- 20230531_HTML_CSS/20230531_default.js
+++ 20230531_HTML_CSS/20230531_default.js
@@ -3,13 +3,13 @@ var forn = cels * (9/5)+32;
 
 document.write("화씨 : "+forn);*/
 
-var num1 = 10;
-var num2 = 20;
+var num1 = 20;
+var num2 = 10;
 var num3 = "10";
 var num4 = 20;
 var num5 = 15;
 
-console.log("일반 크기비교");
+console.log("비교연산자");
 console.log(num1 > num2,num1, ">" ,num2);
 console.log(num1 >= num2,num1, ">=" ,num2);
 console.log(num1 < num2,num1, "<" ,num2);
@@ -23,4 +23,9 @@ console.log("===")
 console.log(num1 === num3,num1," === ",num3);
 console.log(num2 === num3,num2," === ",num3);
 console.log(num1 == num3,num1," == ",num3);
+
+console.log("논리연산자")
+console.log("num1 > num2 && num1 < num5 : ",num1 > num2 && num1 < num5);
+console.log("num1 > num2 || num1 < num5 : ",num1 > num2 || num1 < num5);
+console.log("num1 > num2 && num1 < num5 : ",num1 > num2 && !(num1 < num5));
 
