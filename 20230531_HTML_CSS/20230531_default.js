f44fe6d38b0a683b6f101657d002db8e247a6f81
Hanminho <hihi6024@gmail.com>
2023-05-31 09:48:22 +0900

    20230531 조건부제어문

--- 20230531_HTML_CSS/20230531_default.js
+++ 20230531_HTML_CSS/20230531_default.js
@@ -3,7 +3,7 @@ var forn = cels * (9/5)+32;
 
 document.write("화씨 : "+forn);*/
 
-var num1 = 20;
+/*var num1 = 20;
 var num2 = 10;
 var num3 = "10";
 var num4 = 20;
@@ -28,4 +28,28 @@ console.log("논리연산자")
 console.log("num1 > num2 && num1 < num5 : ",num1 > num2 && num1 < num5);
 console.log("num1 > num2 || num1 < num5 : ",num1 > num2 || num1 < num5);
 console.log("num1 > num2 && num1 < num5 : ",num1 > num2 && !(num1 < num5));
+*/
 
+var num1 = parseInt(prompt("첫번째 숫자 입력 : "));
+var num2 = parseInt(prompt("두번째 숫자 입력 : "));
+
+console.log("=========================");
+if (num1>num2){
+    console.log("큰 수는 num1 : " + num1);
+}
+if(num1<num2){
+    console.log("큰 수는 num2 : " + num2);
+}
+if(num1 == num2){
+    console.log("같은 수");
+}
+/*코드가 간결해짐*/
+console.log("=========================");
+if(num1>num2){
+    console.log("큰 수 num1 : "+ num1);
+}else if(num1 < num2){
+    console.log("큰 수 num2 : "+ num2);
+}else{
+    console.log("같은 수");
+}
+console.log("=========================");
