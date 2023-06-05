3ab24ae456f9b3dd8ab91192061271cca436574f
Hanminho <hihi6024@gmail.com>
2023-06-05 13:49:08 +0900

    20230605 부정연산자(not in & is not)

--- 20230605_HTML_CSS/notInOperator.py
+++ 20230605_HTML_CSS/notInOperator.py
@@ -7,3 +7,9 @@ print("to" not in str2)
 
 str3 = "Wellcome to 멋쟁이 사자"
 print("sub" not in str3)
+a = 10
+b = 10
+c = 10
+d = '10'
+print("a is not b : ",a is not b)
+print("c is not d : ",c is not d)
