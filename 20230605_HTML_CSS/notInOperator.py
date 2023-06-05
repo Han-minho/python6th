aeb24c2978c087638a00fdfb6df7a037a2eda20c
Hanminho <hihi6024@gmail.com>
2023-06-05 13:46:17 +0900

    20230605 부정연산자 파일명

---
+++ 20230605_HTML_CSS/notInOperator.py
@@ -0,0 +1,9 @@
+# not or in(문자열 검증)
+str1 = "Wellcome to 멋쟁이 사자"
+print("to" not in str1)
+
+str2 = "Wellcome top 멋쟁이 사자"
+print("to" not in str2)
+
+str3 = "Wellcome to 멋쟁이 사자"
+print("sub" not in str3)
