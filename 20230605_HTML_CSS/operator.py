a18205fd07df260a669fd51b78a9139813ba459c
Hanminho <hihi6024@gmail.com>
2023-06-05 11:31:21 +0900

    20230605 연산자

---
+++ 20230605_HTML_CSS/operator.py
@@ -0,0 +1,28 @@
+a = 4
+b = 2
+
+total1 = a+b
+total2 = a-b
+total3 = a*b
+total4 = a/b
+total5 = a%b
+total6 = a**b
+total7 = a//b
+total8 = -a//b
+
+#덧셈
+print("total a+b : ",total1,type(total1))
+# 뺄셈
+print("total a-b : ",total2,type(total2))
+# 곱셈
+print("total a*b : ",total3,type(total3))
+# 나눗셈
+print("total a/b : ",total4,type(total4))
+# 나머지
+print("total a%b : ",total5,type(total5))
+# 거듭 제곱
+print("total a**b : ",total6,type(total6))
+# 양수 몫
+print("total a//b : ",total7,type(total7))
+# 음수 몫
+print("total -a//b : ",total8,type(total8))
