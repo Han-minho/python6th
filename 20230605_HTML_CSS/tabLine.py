667897fdc2d658f52cc549922aac6da507e96926
Hanminho <hihi6024@gmail.com>
2023-06-05 15:25:32 +0900

    20230605 print문 출력학습(3)

---
+++ 20230605_HTML_CSS/tabLine.py
@@ -0,0 +1,9 @@
+# 줄 바꿈 대신 tab 사용
+data = [10,20,-50,23.3,'LikeLion']
+
+print(data)
+print('Like','Share','Subscribe',sep='')
+print('Like','Share','Subscribe',sep=' ')
+print('Like','Share','Subscribe',sep='***')
+print('Like','Share','Subscribe',end='\t')
+print('Like','Share','Subscribe',end='\t',sep='\n')
