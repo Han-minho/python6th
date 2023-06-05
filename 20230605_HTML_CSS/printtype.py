e382155122c4031633fdf3a21da51b4abe343734
Hanminho <hihi6024@gmail.com>
2023-06-05 15:25:15 +0900

    20230605 print문 출력학습(2)

--- 20230605_HTML_CSS/printtype.py
+++ 20230605_HTML_CSS/printtype.py
@@ -2,3 +2,10 @@ print('1')
 print('2',end=" ")
 print('3',end=" ")
 print('4')
+
+data = [10,20,-50,23.3,'LikeLion']
+
+print(data)
+print('Like','Share','Subscribe',sep='')
+print('Like','Share','Subscribe',sep=' ')
+print('Like','Share','Subscribe',sep='***')
