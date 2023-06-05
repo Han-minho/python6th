a14b61f036f60149e2b48c2a30319fd3430c5669
Hanminho <hihi6024@gmail.com>
2023-06-05 10:18:10 +0900

    20230605 변수 타입

---
+++ 20230605_HTML_CSS/integer.py
@@ -0,0 +1,17 @@
+# 변수 선언
+a = 10
+b = 3.14
+c = "Hello, World!"
+d = [1,2,3]
+e = (4,5,6)
+f = {7,8,9}
+g = {"apple" : 1, "banana" : 2, "orange" : 3}
+
+# 데이터 타입 출력
+print("a : ",type(a)) #<class 'int'>
+print("b : ",type(b)) #<class 'float'>
+print("c : ",type(c)) #<class 'str'>
+print("d : ",type(d)) #<class 'list'>
+print("e : ",type(e)) #<class 'turple'>
+print("f : ",type(f)) #<class 'set'>
+print("g : ",type(g)) #<class 'dict'>
