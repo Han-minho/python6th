29afd7d29acb4ad5f0251ba623ffb19a4887270b
Hanminho <hihi6024@gmail.com>
2023-06-05 10:21:18 +0900

    20230605 변수 타입

--- 20230605_HTML_CSS/integer.py
+++ 20230605_HTML_CSS/integer.py
@@ -8,10 +8,10 @@ f = {7,8,9}
 g = {"apple" : 1, "banana" : 2, "orange" : 3}
 
 # 데이터 타입 출력
-print("a : ",type(a)) #<class 'int'>
-print("b : ",type(b)) #<class 'float'>
-print("c : ",type(c)) #<class 'str'>
-print("d : ",type(d)) #<class 'list'>
-print("e : ",type(e)) #<class 'turple'>
-print("f : ",type(f)) #<class 'set'>
-print("g : ",type(g)) #<class 'dict'>
+print("a : ",type(a))  #<class 'int'>
+print("b : ",type(b))  #<class 'float'>
+print("c : ",type(c))  #<class 'str'>
+print("d : ",type(d))  #<class 'list'>
+print("e : ",type(e))  #<class 'turple'>
+print("f : ",type(f))  #<class 'set'>
+print("g : ",type(g))  #<class 'dict'>
