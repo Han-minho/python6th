a5b51d3bf6149a4efe047b4873be9f000cea2635
Hanminho <hihi6024@gmail.com>
2023-06-05 12:51:31 +0900

    20230605 비교연산자

---
+++ 20230605_HTML_CSS/logicalOperator.py
@@ -0,0 +1,13 @@
+a = 5
+b = 2
+value1 = a < b
+value2 = a > b
+value3 = a >= b
+value4 = a == b
+value5 = a != b
+
+print("a : ",a,", b : ",b,", a < b : ",value1)
+print("a : ",a,", b : ",b,", a > b : ",value2)
+print("a : ",a,", b : ",b,", a >= b : ",value3)
+print("a : ",a,", b : ",b,", a == b : ",value4)
+print("a : ",a,", b : ",b,", a != b : ",value5)
