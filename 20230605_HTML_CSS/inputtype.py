141574d01c14ffc47a4802c09ca290661cfca783
Hanminho <hihi6024@gmail.com>
2023-06-05 15:40:40 +0900

    20230605 input 출력 학습(1)

--- 20230605_HTML_CSS/inputtype.py
+++ 20230605_HTML_CSS/inputtype.py
@@ -4,3 +4,14 @@ age = int(input('나이를 입력하세요 : '))
 
 print('name : ',name,"age : ",age)
 print("나이의 타입은 : ",type(age))
+
+name = input()
+print(name)
+name = input("Your name : ")
+mobile = input("Enter Your Mobile Number : ")
+mb = int(mobile)
+print(mb,type(mb))
+
+price = float(input("total Price : "))
+
+print(price)
