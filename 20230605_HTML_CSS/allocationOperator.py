8ece2dccbeb5480fdb2f5a0e7067057c97d46f9b
Hanminho <hihi6024@gmail.com>
2023-06-05 13:46:08 +0900

    20230605 비트연산자 파일명

---
+++ 20230605_HTML_CSS/allocationOperator.py
@@ -0,0 +1,35 @@
+a = 10
+b = 20
+m = 15
+
+y = a + b
+print(y)
+
+m += 10
+print(m)
+
+m-=10
+print(m)
+
+m *=10
+print(m)
+
+m /= 10
+print(m)
+
+m%= 10
+print(m)
+
+m **=10
+print(m)
+
+m //= 10
+print(m)
+
+print('bin(a) : ',bin(a))
+print("hex(a) : ",hex(a))
+print('bin(b) : ',bin(b))
+print('hex(b) : ',hex(b))
+
+print('~a : ',~a)
+print('~b : ',~b)
