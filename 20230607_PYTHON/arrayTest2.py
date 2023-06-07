3a7530c66656e0d90cb2eeaadc3abd367e3d3145
Hanminho <hihi6024@gmail.com>
2023-06-07 11:40:48 +0900

    20230607 arrayTest 학습(3)

--- 20230607_PYTHON/arrayTest2.py
+++ 20230607_PYTHON/arrayTest2.py
@@ -16,3 +16,24 @@ while i < n:
     print(stu_roll[i])
     i += 1
 
+print("array element remove")
+
+stu_roll.remove(107)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(stu_roll)
+    i += 1
+
+
+print("array element pop")
+
+stu_roll.pop(2)
+n = len(stu_roll)
+i = 0
+while i < n:
+    print(i,stu_roll)
+    stu_roll.pop()
+    i += 1
+
+print(stu_roll)
