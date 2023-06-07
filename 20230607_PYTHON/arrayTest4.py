00d2374e552346a08657fcc81b5848c741f2de40
Hanminho <hihi6024@gmail.com>
2023-06-07 14:52:26 +0900

    20230607 split,join,startswith,endswith 학습

---
+++ 20230607_PYTHON/arrayTest4.py
@@ -0,0 +1,7 @@
+s = "Hello,World"
+print(s.replace("world".title(), "there"))
+# print(s.split(","))
+split_str = s.split(",")
+print(" ".join(split_str))
+print(s.startswith("Hello"))
+print(s.endswith("Hello"))
