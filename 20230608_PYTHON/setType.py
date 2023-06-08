e98d5c2dd6de49e0c398e874699c87aa4f5b286d
Hanminho <hihi6024@gmail.com>
2023-06-08 14:22:31 +0900

    20230608 set 학습(1)

--- 20230608_PYTHON/setType.py
+++ 20230608_PYTHON/setType.py
@@ -2,6 +2,8 @@ a = {10,20,30}
 a = {10,20,30,'멋쟁이사자','Bae',40}
 a = {10,20,30,'멋쟁이사자','Bae',40,10,20}
 
+new_set = a.copy()
+
 b = set()
 print(type(a))
 
@@ -13,6 +15,22 @@ a.discard('멋쟁이사자')
 a.discard(70)
 print(a)
 
-new_set = a.copy()
-new_set.clear()
-print(new_set)
+# new_set.clear()
+# print(new_set)
+
+intersection_a_new = a.intersection(new_set,a)
+print("intersection_a : ",intersection_a_new)
+
+union_a = a.union(new_set)
+print("union_a : ",union_a)
+
+difference_a = a.difference((new_set))
+print("difference_a : ",difference_a)
+
+print(b.issubset(a))
+print(a.issuperset(b))
+
+#대칭 차집합
+sym_a = a.symmetric_difference(new_set)
+print(sym_a)
+
