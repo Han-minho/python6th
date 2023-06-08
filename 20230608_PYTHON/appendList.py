f75dd9a1f714376b8839d05161d81911c739db9e
Hanminho <hihi6024@gmail.com>
2023-06-08 17:57:41 +0900

    20230608 주석해제

--- 20230608_PYTHON/appendList.py
+++ 20230608_PYTHON/appendList.py
@@ -1,27 +1,27 @@
 fruits = ["apple","banana","charry","orange"]
-# print(fruits)
+print(fruits)
 
 # 리스트 요소 추가
 
-# fruits.append("grape")
-# print(fruits)
-# fruits.insert(2,"kiwi")
-# print(fruits)
-# fruits.pop()
-# print(fruits.pop())
-# print(fruits)
-# print(fruits.pop(1))
-# fruits.remove("charry")
-# print(fruits)
-# fruits.append("charry")
-# print(fruits)
-# fruits.remove("charry")
-# print(fruits)
+fruits.append("grape")
+print(fruits)
+fruits.insert(2,"kiwi")
+print(fruits)
+fruits.pop()
+print(fruits.pop())
+print(fruits)
+print(fruits.pop(1))
+fruits.remove("charry")
+print(fruits)
+fruits.append("charry")
+print(fruits)
+fruits.remove("charry")
+print(fruits)
 
-#valueError
-# print(fruits.index("charray"))
-# print(fruits.remove("charray"))
-# print(fruits.index("charray"))
-# print(fruits.remove("charray"))
-# print(fruits.index("charray"))
+# valueError
+print(fruits.index("charray"))
+print(fruits.remove("charray"))
+print(fruits.index("charray"))
+print(fruits.remove("charray"))
+print(fruits.index("charray"))
 
