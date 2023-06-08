738ba6eab5ce5fcd154f284643c0168e57e39280
Hanminho <hihi6024@gmail.com>
2023-06-08 12:47:20 +0900

    20230608 사용자 입력으로 리스트 추가

---
+++ 20230608_PYTHON/inputList.py
@@ -0,0 +1,10 @@
+fruits = ["apple","banana","charry","orange"]
+#사용자 입력으로 리스트 얻기
+user_input_list = []
+num_elements = int(input("Enter Number of Element : "))
+for i in range(num_elements):
+    user_input_list.append((input("Enter Element : ")))
+
+print("User Input List : ")
+for element in user_input_list:
+    print(element)
