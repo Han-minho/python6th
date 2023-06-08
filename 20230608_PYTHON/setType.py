f8c0d55751433fa39d84483070e6eeb624588331
Hanminho <hihi6024@gmail.com>
2023-06-08 14:11:49 +0900

    20230608 set 학습

---
+++ 20230608_PYTHON/setType.py
@@ -0,0 +1,18 @@
+a = {10,20,30}
+a = {10,20,30,'멋쟁이사자','Bae',40}
+a = {10,20,30,'멋쟁이사자','Bae',40,10,20}
+
+b = set()
+print(type(a))
+
+a.add(50)
+a.update([10,20,60,70])
+print(a)
+a.remove('멋쟁이사자')
+a.discard('멋쟁이사자')
+a.discard(70)
+print(a)
+
+new_set = a.copy()
+new_set.clear()
+print(new_set)
