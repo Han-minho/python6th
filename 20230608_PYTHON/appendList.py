71ab32068acb89a0e099f06bf030b8f86e392f25
Hanminho <hihi6024@gmail.com>
2023-06-08 12:47:08 +0900

    20230608 append로 리스트 추가

---
+++ 20230608_PYTHON/appendList.py
@@ -0,0 +1,27 @@
+fruits = ["apple","banana","charry","orange"]
+# print(fruits)
+
+# 리스트 요소 추가
+
+# fruits.append("grape")
+# print(fruits)
+# fruits.insert(2,"kiwi")
+# print(fruits)
+# fruits.pop()
+# print(fruits.pop())
+# print(fruits)
+# print(fruits.pop(1))
+# fruits.remove("charry")
+# print(fruits)
+# fruits.append("charry")
+# print(fruits)
+# fruits.remove("charry")
+# print(fruits)
+
+#valueError
+# print(fruits.index("charray"))
+# print(fruits.remove("charray"))
+# print(fruits.index("charray"))
+# print(fruits.remove("charray"))
+# print(fruits.index("charray"))
+
