fee68f987b23a6ba6bc386c7ab24c35323b8974b
Hanminho <hihi6024@gmail.com>
2023-06-08 13:21:47 +0900

    20230608 alphabot generater

---
+++ 20230608_PYTHON/alphabotGenerateList.py
@@ -0,0 +1,11 @@
+def generate_alphabet(start_letter,end_letter):
+    start = ord(start_letter)
+    end = ord(end_letter)
+    while start <= end:
+        yield chr(start)
+        start += 1
+
+runner = generate_alphabet('A','F')
+for letter in runner:
+    print(letter)
+
