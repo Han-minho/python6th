da88ac9f8fea2dff062d2d0708ac726d3ab7c09a
Hanminho <hihi6024@gmail.com>
2023-06-09 11:28:49 +0900

    20230609 상속2

---
+++ 20230609_PYTHON/InheritanceTest2.py
@@ -0,0 +1,35 @@
+class ParentClass :
+    def __init__(self):
+        self.name = 'parent'
+        self.number = 10
+
+    def __str__(self):
+        return f'ParentClass name : {self.name},number : {self.number}'
+
+    def add_num(self, new_number):
+        print('부모 : ', new_number, '만큼 더 해야지')
+        self.number = self.number + 7
+
+class ChildClass(ParentClass):
+       def __init__(self):
+           super().__init__()
+           self.name = 'child'
+
+       def __str__(self):
+           return f'ChildClass name : {self.name},number : {self.number}'
+       def add_num(self, new_number):
+           print('말 안듣는 자식 : 고정적으로 5 더할건데?')
+           self.number = self.number+5
+
+parent = ParentClass()
+child =  ChildClass()
+# print(parent)
+# print(child)
+
+print('7을 더하세요')
+parent.add_num(7)
+child.add_num(7)
+print('=========================')
+print(parent)
+print(child)
+print('=========================')
