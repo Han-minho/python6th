60aa52eed239534b4b5424fe2d8a3a4feda21953
Hanminho <hihi6024@gmail.com>
2023-06-09 11:04:10 +0900

    20230609 날짜 print 내용 학습 수정

--- 20230609_PYTHON/epochTest.py
+++ 20230609_PYTHON/epochTest.py
@@ -7,12 +7,15 @@ import time
 from datetime import datetime
 from datetime import date
 
+
 dt = datetime(year=2023,month=5,day=5,hour=10,minute=30)
 print(dt)
 print(type(dt))
 
+current_time = time.ctime()
 current_datetime = datetime.now()
-print(current_datetime)
+print("current_datetime : ",current_datetime)
+print("current_time : ",current_time)
 
 d = date(year=2023,month=6,day=25)
 print(d)
