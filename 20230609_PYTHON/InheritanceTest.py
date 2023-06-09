1958bf2a08b1a29275aaa29215ecc13ac2c4e4ad
Hanminho <hihi6024@gmail.com>
2023-06-09 09:48:31 +0900

    20230609 Inheritance 학습

---
+++ 20230609_PYTHON/InheritanceTest.py
@@ -0,0 +1,27 @@
+class Mobile:
+    fp = 'yes'
+
+readlme = Mobile()
+redme = Mobile()
+geek = Mobile()
+
+print("===========================")
+print("Mobile : ",Mobile.fp)
+print("readlme : ",readlme.fp)
+print("redme :",redme.fp)
+print("geek : ",geek.fp)
+
+Mobile.fp = 'no'
+
+print("===========================")
+print("readlme : ",readlme.fp)
+print("redme :",redme.fp)
+print("geek : ",geek.fp)
+print("===========================")
+readlme.fp = "Not Working"
+print("Mobile : ",Mobile.fp)
+print("readlme : ",readlme.fp)
+print("redme :",redme.fp)
+print("geek : ",geek.fp)
+print("===========================")
+
