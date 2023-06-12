bf4cf176009768fdfbc0ac199a1c4b19c1c996b5
Hanminho <hihi6024@gmail.com>
2023-06-12 13:11:35 +0900

    20230612 errorHandling 실습(1)

---
+++ 20230612_PYTHON/errorHandlingTest2.py
@@ -0,0 +1,7 @@
+try :
+    number  = int("Not a number")
+    #number = 5 + "Not a number"
+except ValueError :
+    print("Error : Invalid value.")
+except TypeError :
+    print("Error : Invalid type.")
