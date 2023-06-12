8094df8b7a8fc139aee352c9108d2698d68f80c6
Hanminho <hihi6024@gmail.com>
2023-06-12 10:32:18 +0900

    20230612 상속 실습

---
+++ 20230612_PYTHON/InheritanceTest01.py
@@ -0,0 +1,22 @@
+# 부모 클래스
+class Vehicle:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def start_engine(self):
+        return "The engine is running!"
+
+#자식 클래스
+class Car(Vehicle):
+    def start_engine(self):
+        return super().start_engine() + " It`s a car engine"
+
+# 인스턴스 생성
+my_car = Car("Toyota","Corolla",2020)
+
+#메소드 호출
+print(my_car.start_engine())
+
+#출력 : The engine is running! It`s a car engine
