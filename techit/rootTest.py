468352616ec4a530a07c5e1cf3c479e545f50f9c
Hanminho <hihi6024@gmail.com>
2023-06-13 15:27:14 +0900

    20230612 techit 온라인 강의 학습(5) for문

---
+++ techit/rootTest.py
@@ -0,0 +1,17 @@
+# for x in range(30):
+#     print(x)
+
+foods = ["된장찌개","피자","제육볶음","순대국"]
+
+# for x in range(3):
+#     print(foods[x])
+
+# list 출력 제한 없음
+# for x in foods:
+#     print(x)
+
+information = {"고향":"수원", "취미":"영화관람","좋아하는 음식":"국수"}
+for x,y in information.items():
+    print(x) # keys output
+    print(y) # value output
+
