33a04139bb155c234609cd115431f03fc7488693
Hanminho <hihi6024@gmail.com>
2023-06-14 18:07:15 +0900

    20230614 techit 동영상 강의 학습내용(5) roop test

---
+++ techit/0614/roopTest.py
@@ -0,0 +1,63 @@
+# 증가
+# print("=====================")
+# i = 0
+# while i < 6:
+#     print(i)
+#     i = i+1
+
+# 감소
+# print("=====================")
+# j = 5
+# while j >= 0:
+#     print(j)
+#     j = j - 1
+
+# print("=====================")
+
+# i = 0
+# while True:
+#     print(i)
+#     i = i + 1
+#
+#     if i >= 3:
+#         print("if문 출력")
+#         break
+#     else:
+#         continue
+#
+# print("반복문 종료!")
+
+# print("=====================")
+# i = 0
+# while i < 10:
+#     i = i + 1
+#
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+# print("반복 종료!")
+
+print("=====================")
+
+# for x in range(100):
+#     print("안녕하세요")
+
+# for x in range(10,20):
+#     print(x)
+
+# for x in range(2,21,2):
+#     print(x)
+
+# result = 0
+#
+# for x in range(1,101):
+#     result = result + x
+#
+# print(result)
+#
+# result1 = 1
+#
+# for x in range(1,11):
+#     result1 = result1 * x
+#
+# print(result1)
