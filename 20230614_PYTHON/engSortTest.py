8ca77ef13d4eac6da3c4af70b01ffcce6408815e
Hanminho <hihi6024@gmail.com>
2023-06-15 09:34:02 +0900

    20230614 영문 sort 학습

---
+++ 20230614_PYTHON/engSortTest.py
@@ -0,0 +1,29 @@
+eng_list = ["apple","banana","kiwi","tomato","graph","male","waterMale"]
+
+# 오름차순
+eng_list.sort()
+print("오름차순")
+print(eng_list)
+
+# 내림차순
+print("내림차순")
+eng_list.sort(reverse=True)
+print(eng_list)
+kor_list = ["한민호","고길동","김철수","김영희","박훈","나유리","나미리","이연결"]
+#오름차순
+print("오름차순")
+kor_list.sort()
+print(kor_list)
+print("내림차순")
+kor_list.sort(reverse=True)
+print(kor_list)
+
+list_number = [4,1,3,2,5,7,9,8,6]
+
+print("오름차순")
+list_number.sort()
+print(list_number)
+
+list_number.sort(reverse=True)
+print(list_number)
+
