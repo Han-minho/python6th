561ec086914de4492907effb7d40e869aad428ed
Hanminho <hihi6024@gmail.com>
2023-06-19 15:57:39 +0900

    20230619 http download test

--- 20230619_PYTHON/requestTest2.py
+++ 20230619_PYTHON/requestTest2.py
@@ -1,5 +1,7 @@
-from urllib.request import urlopen
+import http.client
+from urllib.request import urlopen, urlretrieve
 from html.parser import HTMLParser
+from pathlib import Path
 
 class ImageParser(HTMLParser):
     def __init__(self):
@@ -18,28 +20,50 @@ class ImageParser(HTMLParser):
 
 def parse_image(data):
 
-    """
-        주어진 HTML 데이터를 파싱하고 모든 이미지 URL을 추출합니다.
-
-        Args :
-            data : 파싱할 HTML 데이터
-        Returns:
-            이미지 URL 집합.
-    """
-
     parser = ImageParser()   # parser 인스턴스를 만듭니다.
     parser.feed(data)
     return parser.result
 
+def download_Image(url,data):
+    download_dir = Path('DOWNLOAD')
+    download_dir.mkdir(exist_ok=True)
+
+    parser = ImageParser()
+    parser.feed(data)
+    data_set = set(x for x in parser.result)
+    for x in sorted(data_set):
+        from urllib.parse import urljoin
+        image_url = urljoin(url,x)
+        basename = Path(image_url).name
+        target_file = download_dir / basename
+        print(target_file)
+
+        print("Downloading........",image_url)
+        urlretrieve(image_url,target_file)
+
+
 def main():
+    # url = "https://www.google.co.kr"
+    # with urlopen(url) as f:
+        # charset = f.headers.getparam('charset')
+        # data = f.read().decode(charset)
+    host = "www.google.co.kr"
+    conn = http.client.HTTPConnection(host)
+    conn.request('GET','')
+    resp = conn.getresponse()
 
-    url = "https://www.google.co.kr"
-    with urlopen(url) as f:
-        charset = f.headers.getparam('charset')
-        data = f.read().decode(charset)
-        dataset = parse_image(data)
-        print("Fetched images from",url)
-        print(",".join(sorted(dataset)))
+    charset = resp.msg.get_param('charset')
+    data = resp.read().decode(charset)
+    conn.close()
+    print('\n>>>>>>> Downloading........',host)
+    from urllib.parse import urlunparse
+    url = urlunparse(('http',host,'','','',''))
 
-    if __name__ == "__main__":
-        main()
+    dataset = parse_image(data)
+    print("Fetched images from",host)
+    print(",".join(sorted(dataset)))
+    download_Image(url,data)
+
+if __name__ == "__main__":
+    main()
+
